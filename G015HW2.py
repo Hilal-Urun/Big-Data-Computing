9977a5b816fd0622fd0f07a20398b1e617b36147
hilalurun <hilalurun07@gmail.com>
2023-05-16 22:43:36 +0200

    small additions until tomorrow :)

--- G015HW2.py
+++ G015HW2.py
@@ -9,6 +9,40 @@ import statistics
 import time
 
 
+def countTriangles2(colors_tuple, edges, rand_a, rand_b, p, num_colors):
+    # We assume colors_tuple to be already sorted by increasing colors. Just transform in a list for simplicity
+    colors = list(colors_tuple)
+    # Create a dictionary for adjacency list
+    neighbors = defaultdict(set)
+    # Creare a dictionary for storing node colors
+    node_colors = dict()
+    for edge in edges:
+        u, v = edge
+        node_colors[u] = ((rand_a * u + rand_b) % p) % num_colors
+        node_colors[v] = ((rand_a * v + rand_b) % p) % num_colors
+        neighbors[u].add(v)
+        neighbors[v].add(u)
+
+    # Initialize the triangle count to zero
+    triangle_count = 0
+
+    # Iterate over each vertex in the graph
+    for v in neighbors:
+        # Iterate over each pair of neighbors of v
+        for u in neighbors[v]:
+            if u > v:
+                for w in neighbors[u]:
+                    # If w is also a neighbor of v, then we have a triangle
+                    if w > u and w in neighbors[v]:
+                        # Sort colors by increasing values
+                        triangle_colors = sorted((node_colors[u], node_colors[v], node_colors[w]))
+                        # If triangle has the right colors, count it.
+                        if colors == triangle_colors:
+                            triangle_count += 1
+    # Return the total number of triangles in the graph
+    return triangle_count
+
+
 def CountTriangles(edges):
     # Create a defaultdict to store the neighbors of each vertex
     neighbors = defaultdict(set)
@@ -33,6 +67,7 @@ def CountTriangles(edges):
     # Return the total number of triangles in the graph
     return triangle_count
 
+
 def MR_ApproxTCwithNodeColors(edges, C):
     """
     input:
@@ -44,29 +79,35 @@ def MR_ApproxTCwithNodeColors(edges, C):
 
     """
     p = 8191
-    a = rand.randint(1, p-1)
-    b = rand.randint(0, p-1)
+    a = rand.randint(1, p - 1)
+    b = rand.randint(0, p - 1)
 
     def hash(u):
         return ((((a * u) + b) % p) % C)
-    
+
     def edge_same_color(hash: callable, edge):
         colors = hash(edge[0]), hash(edge[1])
         if colors[0] == colors[1]:
             return [(colors[0], edge)]
         else:
             return []
-    
+
     triangle_count = (edges.flatMap(lambda edge: edge_same_color(hash, edge))  # <-- MAP PHASE (R1)
-                    .groupByKey()  # <-- GROUPING
-                    .map(lambda edges: (0, CountTriangles(edges[1])))  # <-- REDUCE PHASE (R1)
-                    .reduceByKey(lambda x,y: x + y))  # <-- REDUCE PHASE (R2)
+                      .groupByKey()  # <-- GROUPING
+                      .map(lambda edges: (0, CountTriangles(edges[1])))  # <-- REDUCE PHASE (R1)
+                      .reduceByKey(lambda x, y: x + y))  # <-- REDUCE PHASE (R2)
 
     return triangle_count.collect()[0][1] * C ** 2
 
+
+def MR_ExactTC(rdd_edges, C):
+    count = ""
+    return count
+
+
 def main():
-    # CHECKING NUMBER OF CMD LINE PARAMTERS
-    assert len(sys.argv) == 4, "Usage: python G015HW1.py <C> <R> <file_name>"
+    # CHECKING NUMBER OF CMD LINE PARAMETERS
+    assert len(sys.argv) == 4, "Usage: python G015HW1.py <C> <R> <F> <file_name>"
 
     # SPARK SETUP
     conf = SparkConf().setAppName('TriangleCountExample').setMaster("local[*]")
@@ -84,14 +125,23 @@ def main():
     assert R.isdigit(), "R must be an integer"
     R = int(R)
 
-    # 3. Read input file and subdivide it into C partitions
-    data_path = sys.argv[3]
-    assert os.path.isfile(data_path), "File or folder not found"
-    rawData = sc.textFile(data_path)  
-    # transform it into an RDD of edges
-    edges = rawData.map(lambda line: tuple(map(int, line.split(",")))).sortBy(lambda _: random.random()) #shuffle the data
-    edges = edges.repartition(numPartitions=C).cache() 
+    # 3. Read the binary flag
+    F = sys.argv[3]
+    assert F in ['0', '1'], "F must be either 0 or 1"
+    F = int(F)
 
+    # 3. Read input file and subdivide it into C partitions
+    data_path = sys.argv[4]
+    assert os.path.isfile(data_path), "File or folder not found"
+    rawData = sc.textFile(data_path)
+    # transform it into an RDD of edges
+    # edges = rawData.map(lambda line: tuple(map(int, line.split(",")))).sortBy(
+    #     lambda _: random.random())  # shuffle the data
+    # edges = edges.repartition(numPartitions=C).cache()
+    edges = rawData.map(lambda line: line.split()) \
+        .map(lambda pair: (int(pair[0]), int(pair[1]))) \
+        .partitionBy(32) \
+        .cache()
 
     # required prints in the report
     print("Dataset = ", os.path.basename(data_path))
@@ -100,18 +150,30 @@ def main():
     print("Number of Edges = ", numEdges)
     print("Number of Colors = ", C)
     print("Number of Repetitions = ", R)
-
-    # Approximation through node coloring
-    print("Approximation through node coloring")
-    node_color_estimates = []
-    times = 0
-    for i in range(R):
-        start_time = time.time()
-        node_color_estimates.append(MR_ApproxTCwithNodeColors(edges, C))
-        finish_time = time.time()
-        times += (finish_time - start_time)
-    print(f'- Number of triangles (median over {R} runs) = ', statistics.median(node_color_estimates)) # get the median
-    print(f'- Running time (average over {R} runs) = ', int((times / R) * 1000), "ms") # get the average time
+    if F is 0:
+        # Approximation through node coloring
+        print("Approximation through node coloring")
+        node_color_estimates = []
+        times = 0
+        for i in range(R):
+            start_time = time.time()
+            node_color_estimates.append(MR_ApproxTCwithNodeColors(edges, C))
+            finish_time = time.time()
+            times += (finish_time - start_time)
+        print(f'- Number of triangles (median over {R} runs) = ',
+              statistics.median(node_color_estimates))  # get the median
+        print(f'- Running time (average over {R} runs) = ', int((times / R) * 1000), "ms")  # get the average time
+    if F is 1:
+        # Exact through node coloring
+        print("Exact algorithm with node coloring")
+        times = 0
+        for i in range(R):
+            start_time = time.time()
+            exact_triangles_count = MR_ExactTC()
+            finish_time = time.time()
+            times += (finish_time - start_time)
+        print(f'- Number of triangles = ', exact_triangles_count)
+        print(f'- Running time (average over {R} runs) = ', int((times / R) * 1000), "ms")  # get the average time
 
 
 if __name__ == "__main__":
